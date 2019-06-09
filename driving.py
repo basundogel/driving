ac0602aaad894886ae403191c18721fa98b782e1
mamaka <basundogel@gmail.com>
2019-06-09 12:09:01 +0800

    add TW and US

---
+++ driving.py
@@ -0,0 +1,14 @@
+country = input('請輸入您的國家:')
+age = input('請輸入您的年齡:')
+age = int('age')
+if country == '台灣':
+	if age >= 18:
+		print('你可以考駕照唷>.^')
+	elif:
+		print('你還不能考駕照sorry啦 :P')
+if country == '美國':
+	if age >= 16:
+		print('你可以考駕照唷>.^')
+	elif:
+		print('你還不能考駕照sorry啦 >__<')
+		
