51144387e5e80a0de4676060da78ee52f59e1549
mamaka <basundogel@gmail.com>
2019-06-09 12:22:12 +0800

    add else part

--- driving.py
+++ driving.py
@@ -6,8 +6,11 @@ if country == '台灣':
 		print('你可以考駕照唷>.^')
 	else:
 		print('你還不能考駕照sorry啦 :P')
-if country == '美國':
+elif country == '美國':
 	if age >= 16:
 		print('你可以考駕照唷>.^')
 	else:
 		print('你還不能考駕照sorry啦 >__<')
+else:
+	print('國家請輸入台灣或美國 謝謝!')
+
